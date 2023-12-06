3becf5a8f65274a6aa2fe63de9ce43f035d35133
Kreg Flowers <kflow7@gmail.com>
2023-12-06 05:48:27 -0700

    day6

--- day6.py
+++ day6.py
@@ -4,20 +4,17 @@ times,distances = open('data/day6.txt','r').read().splitlines()
 times = [int(x) for x in times.split(":")[1].split(" ") if x.isdigit()]
 records = [int(x) for x in distances.split(":")[1].split(" ") if x.isdigit()]
 
-score = 1
-for t,r in zip(times,records):
-    ds = 0
-    for t_go in range(t):
-        ds+=1
-    score *= ds
-       
-t = int(''.join([str(x) for x in times]) )
-r = int(''.join([str(x) for x in records]) )
-
 def quad(b,c):
     x1 = (-b + (b**2 - (4*c))**0.5)/2
     x2 = (-b - (b**2 - (4*c))**0.5)/2
     return int(max(x1,x2))-int(min(x1,x2))
+
+score = 1
+for t,r in zip(times,records):
+    score *= quad(-t,r)
+       
+t = int(''.join([str(x) for x in times]) )
+r = int(''.join([str(x) for x in records]) )
 
 print("pt1",score)
 print("pt2",quad(-t,r))
