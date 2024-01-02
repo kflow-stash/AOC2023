1ae7b28d49940848a07c2eb922ff20c84bfd3316
Kreg Flowers <kflow7@gmail.com>
2024-01-01 22:37:23 -0700

    day25

--- day25.py
+++ day25.py
@@ -1,9 +1,7 @@
 from collections import defaultdict
-from copy import deepcopy,copy
-import sys
+from copy import copy
 from sys import maxsize as INT_MAX
 from collections import deque
-sys.setrecursionlimit(10000)
 input_ = open("data/day25.txt", "r").read().splitlines()
 
 nodes = defaultdict(set)
@@ -45,10 +43,8 @@ def shortest_cycle(n1):
    
 paths = set()
 for src in nodes.keys():
-    print(src)
     paths.add(shortest_cycle(src))
     
-
 default_ = list(paths)[0]
 cycle1 = set(default_)
 paths.remove(default_)
@@ -66,58 +62,5 @@ while adding:
 for path in paths:
     cycle2.update(path)
     
-print(len(cycle1),len(cycle2))   
+print(len(cycle1)*len(cycle2))   
     
-snips = list()
-for node1, v in nodes.items():
-    for node2 in v:
-        if (node1 in cycle1 and node2 in cycle2) or (node1 in cycle2 and node2 in cycle1):
-            snips.append((node1,node2))
-        
-
-found_splits = []
-splits = {}
-
-for node1,v in nodes.items():
-    for node2 in v:
-        
-        split = tuple(sorted([node1,node2]))
-        if split not in splits:
-        # get sets of items on either side of the split
-            if split == ('jqt','nvd'):
-                stop_here = 1
-            
-            n_ties = count_ties(split[0],split,net=[split[0]])
-            
-            if n_ties<3:
-                print(split,n_ties)
-            
-            #net1 = get_net(split[0],split[1],net=set())
-            ##print(" ")
-            #net2 = get_net(split[1],split[0],net=set())
-            #inet = net1.intersection(net2)
-            
-            
-            #splits[split] = {'net1':deepcopy(net1),'net2':deepcopy(net2),'inet':deepcopy(inet),'n_inter':len(inet)}
-  
-min_split1 = sorted(splits.items(),key=lambda x: x[1]["n_inter"])[0]
-found_splits.append(min_split1[0])
-
-
-#examine the splits of the intersection
-splits = {}
-for node1 in min_split1[1]["inet"]:
-    for node2 in nodes[node1]:
-        split = tuple(sorted([node1,node2]))
-        if split not in splits:
-        # get sets of items on either side of the split
-            net1 = get_net(split[0],split[1])
-            net2 = get_net(split[1],split[0])
-            inet = net1.intersection(net2)
-            splits[split] = {'net1':net1,'net2':net2,'inet':inet,'n_inter':len(inet)}
-
-min_split = sorted(splits.items(),key=lambda x: x[1]["n_inter"])[0]
-
-stop_here = 1
-        
-
